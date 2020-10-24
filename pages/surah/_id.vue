608981eead61d42c2da4541973898948211064ad
syakirrafaei <syakir.rafaei@gmail.com>
2020-10-24 23:31:16 +0800

    Remove bismillah in first text.

--- pages/surah/_id.vue
+++ pages/surah/_id.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="md:px-64">
     <div class="font-bold text-4xl text-center mb-10">
-      {{ ayahs.englishName }}
+      {{ ayah.englishName }}
     </div>
     <ul v-for="ayah in ayahs.ayahs" :key="ayah.number" class="text-right">
       <span
         class="text-2xl rounded-full px-3 py-1 text-green-500 border-green-600 border text-center"
         >{{ ayah.numberInSurah }}</span
       >
-      <li class="inline-block font-kitab mb-10">
+      <li v-if="ayah.numberInSurah !== 1" class="inline-block font-kitab mb-10">
         {{ ayah.text }}
+      </li>
+      <li
+        v-else-if="ayah.numberInSurah === 1 && firstAyahs.length === 1"
+        class="inline-block font-kitab mb-10"
+      >
+        <span>{{ firstAyahs }} بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ</span>
+      </li>
+      <li v-else class="inline-block font-kitab mb-10">
+        {{ firstAyahs }}
       </li>
     </ul>
   </div>
@@ -20,6 +29,12 @@ export default {
     const res = await $axios.$get(`surah/${params.id}`)
     const ayahs = res.data
     return { ayahs }
+  },
+  computed: {
+    firstAyahs() {
+      const ayahsFirst = this.ayahs.ayahs[0].text
+      return ayahsFirst.replace('بِسْمِ اللَّهِ الرَّحْمَٰنِ الرَّحِيمِ', '')
+    },
   },
 }
 </script>

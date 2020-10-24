90b54c5b15cbc44cf676c9835bf6807464f9d50b
syakirrafaei <syakir.rafaei@gmail.com>
2020-10-24 14:27:17 +0800

    Display one surah.

---
+++ pages/surah/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="md:px-64">
+    <div class="font-bold text-4xl text-center mb-10">
+      {{ ayahs.englishName }}
+    </div>
+    <ul v-for="ayah in ayahs.ayahs" :key="ayah.number" class="text-right">
+      <span
+        class="text-2xl rounded-full px-3 py-1 text-green-500 border-green-600 border text-center"
+        >{{ ayah.numberInSurah }}</span
+      >
+      <li class="inline-block font-kitab mb-10">
+        {{ ayah.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $axios, params }) {
+    const res = await $axios.$get(`surah/${params.id}`)
+    const ayahs = res.data
+    return { ayahs }
+  },
+}
+</script>

71165d5c342a43832c59042ae4e075be65e4c774
syakirrafaei <syakir.rafaei@gmail.com>
2020-10-24 14:26:57 +0800

    Kitab font

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,31 @@
 <template>
   <div>
-    <Nuxt />
+    <NavBar />
+    <div class="min-h-screen mt-20 bg-gray-100 antialiased py-6 px-12">
+      <Nuxt />
+    </div>
   </div>
 </template>
-
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+@font-face {
+  font-family: 'Kitab';
+  src: url('//cdn.alquran.cloud/public/fonts/Kitab-Regular.ttf');
 }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+.font-kitab {
+  font-family: 'Kitab';
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+@font-face {
+  font-family: 'Kitab Bold';
+  src: url('//cdn.alquran.cloud/public/fonts/Kitab-Bold.ttf');
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+.font-kitab-bold {
+  font-family: 'Kitab Bold';
 }
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.font-kitab {
+  font-family: 'Kitab';
+  font-size: 40px;
 }
 </style>

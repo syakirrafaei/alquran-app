5b27daaf1f1988ee42a46d78a6c8febd2868d8b2
syakirrafaei <syakir.rafaei@gmail.com>
2020-10-25 20:43:55 +0800

    Change font styling

--- pages/surah/_id.vue
+++ pages/surah/_id.vue
@@ -8,13 +8,13 @@
       :key="ayah.number"
       class="grid shadow bg-grey grid-rows-2 grid-cols-4 grid-flow-col"
     >
-      <div class="row-span-2 col-span-1 text-center my-auto">
-        <div class="font-bold h-20 w-20 shadow py-4 rounded-full text-4xl">
+      <div class="row-span-2 col-span-1 text-center my-auto px-2">
+        <div class="number-kitab rounded-full">
           {{ ayah.numberInSurah.toLocaleString('ar-EG') }}
         </div>
       </div>
       <!-- <div class="row-span-1 col-span-1 text-center"></div> -->
-      <div class="row-span-2 col-span-3 text-right py-6 px-1 md:px-3">
+      <div class="row-span-2 col-span-3 text-right py-6 pl-1 pr-3 md:px-3">
         <span v-if="ayah.numberInSurah !== 1" class="inline-block font-kitab">
           <Highlight :ayah="ayah.text" />
         </span>

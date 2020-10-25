578fc88327dfa2f4a9e34193bcdf9add0868f5af
syakirrafaei <syakir.rafaei@gmail.com>
2020-10-25 20:22:59 +0800

    Add more menu

--- components/NavMobile.vue
+++ components/NavMobile.vue
@@ -9,7 +9,23 @@
       :class="{ scrolled: !view.atTopOfPage }"
       class="w-full items-center justify-between grid grid-cols-3 py-1 px-3 text-center shadow"
     >
-      <div class="col-span-1"></div>
+      <div class="col-span-1">
+        <svg
+          class="w-6 h-6 mx-auto text-green-500"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+          ></path>
+        </svg>
+        <div class="text-sm text-green-600 antialiased">Dark Mode</div>
+      </div>
       <div class="col-span-1">
         <nuxt-link to="/"
           ><svg
@@ -29,7 +45,23 @@
           <div class="text-sm text-green-600 antialiased">Home</div>
         </nuxt-link>
       </div>
-      <div class="col-span-1"></div>
+      <div class="col-span-1">
+        <svg
+          class="w-6 h-6 mx-auto text-green-500"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"
+          ></path>
+        </svg>
+        <div class="text-sm text-green-600 antialiased">Learn</div>
+      </div>
     </nav>
   </div>
 </template>

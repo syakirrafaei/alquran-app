518a3c60ca77b4ca489dc00844a04fa29ea87aca
syakirrafaei <syakir.rafaei@gmail.com>
2020-11-12 18:09:32 +0800

    Change english to malay

---
+++ pages/belajar.vue
@@ -0,0 +1,9 @@
+<template>
+  <transition name="home">
+    <div
+      class="w-full text-xl hero bg-green-200 p-6 font-bold text-white text-center"
+    >
+      Cara Belajar Memahami al-Quran
+    </div>
+  </transition>
+</template>
